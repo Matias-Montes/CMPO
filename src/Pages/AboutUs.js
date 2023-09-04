07e4f70d3ac362039d8f4cc4f9256b0ca22966fe
Matias-Montes <tute.1986@gmail.com>
2023-09-04 16:03:15 -0300

    cambio de descripcion en about us

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -10,14 +10,22 @@ export const AboutUs = () => {
       
       <div className='aboutus-div_text d-flex flex-column'>
       <h3 className='product-tittles text-center my-4 p-2'><strong> La Empresa </strong> </h3>
-      <p className='aboutus-article_text'>CMPO es una empresa que realiza y distribuye artículos de plástico de primera calidad. Ubicada en la zona oeste de buenos aires, pero con alcance a todo el país.</p>
+      <p className='aboutus-article_text'>CMPO es una empresa Argentina, joven y dinámica, la cual nace con el objetivo de producir artículos plásticos para el hogar y la industria en general.
+</p>
 
-      <p className='aboutus-article_text'>Cuenta con la experiencia y la infraestructura necesaria para atender sus necesidades, tanto para pequeños como para grandes proyectos. </p>
+      <p className='aboutus-article_text'>Diseñamos, desarrollamos y fabricamos todos nuestros artículos con materia prima virgen, utilizando maquinarias de última tecnología para la optimización de nuestros procesos de producción, buscando con ello otorgar productos confiables para nuestros clientes.
+</p>
 
-      <p className='aboutus-article_text'>Atención personalizada al cliente, lo que permite brindarle el mejor servicio para que obtenga un excelente producto y prestaciones.</p> 
+      <p className='aboutus-article_text'>A través de nuestra cadena de comercialización y logística logramos abastecer a pequeños y grandes distribuidores a lo largo de todo el territorio nacional.
+</p> 
+      <p className='aboutus-article_text'>Nuestra misión es crear una marca de referencia para el mercado argentino con un enfoque en la calidad y el diseño de todos nuestros productos.
+
+</p> 
       </div>
     </article>
          
     </section>
   )
 }
+
+
